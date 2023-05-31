susam/devil

commit 60f696842940ee7a57d003f7b740a88ed1dd5d14

Fix translation of function keys

devil.el
@@ -211,7 +211,7 @@ or an undefined key sequence.  If the result is a valid key
 sequence for a special key command or an Emacs command, then the
 command is executed.  Otherwise, this function calls itself
 recursively to read yet another key from the user."
-  (setq key (vconcat key (vector (read-key (devil--make-prompt key)))))
+  (setq key (vconcat key (vector (read-event (devil--make-prompt key)))))
   (unless (devil--run-command key)
     (devil--read-key key)))
 

commit 2925d977feaaf5ae1520aeba70be1e4d34884c73

Fix definition of special key ", <return>"
Commit 60f6968 broke the invocation of special key `, RET` which is
meant to insert comma followed by newline. Prior to that commit,
typing `, <return>` produced the Devil key sequence `, RET` which was
defined as a special key. However with that commit, typing `, <return>`
now produces the Devil key sequence `, <return>`.  Therefore, the
special key definition needs to be updated from `, RET` to `, <return>`.

devil.el
@@ -4,7 +4,7 @@
 
 ;; Author: Susam Pal <susam@susam.net>
 ;; Maintainer: Susam Pal <susam@susam.net>
-;; Version: 0.4.0
+;; Version: 0.5.0.pre1
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: convenience, abbrev
 ;; URL: https://github.com/susam/devil
@@ -113,7 +113,7 @@ activated using the given KEY-SEQUENCE."
 (defvar devil-special-keys
   (list (cons "%k %k" (lambda () (interactive) (devil-run-key "%k")))
         (cons "%k SPC" (lambda () (interactive) (devil-run-key "%k SPC")))
-        (cons "%k RET" (lambda () (interactive) (devil-run-key "%k RET"))))
+        (cons "%k <return>" (lambda () (interactive) (devil-run-key "%k RET"))))
   "Special Devil keys that are executed as soon as they are typed.
 
 The value of this variable is an alist where each key represents